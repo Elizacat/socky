8f9bd971aa4ef829d8b2d08120906c0a9e01c8cc
Elizabeth Myers <elizabeth@interlinked.me>
2013-05-03 08:09:37 -0500

    Refactor code into a class

--- socky.py
+++ socky.py
@@ -13,8 +13,6 @@ import re
 
 # Globals deaugh
 ix = None
-lastsaid = 0
-quitme = False
 
 # XXX hardcoded
 admins = ['Elizacat', 'SilentPenguin']
@@ -26,128 +24,133 @@ def make_query(text):
 
     return Or(orlist)
 
-def handle_quoteadd(instance, line, target, params):
-    # XXX Different types
-    parsed = re.match('\[(.+)\]\W?=\W?\[(.+)\]', params)
+class SockyIRCClient(client.IRCClient):
+    def __init__(self, *args, **kwargs):
+        client.IRCClient.__init__(self, *args, **kwargs)
 
-    if not parsed: return
+        self.quitmme = False
+        self.lastsaid = 0
 
-    trigger, response = parsed.groups()
-    trigger = trigger.lower()
+        self.add_dispatch_in('PRIVMSG', 1000, self.handle_privmsg)
 
-    writer = ix.writer()
-    try:
-        writer.add_document(trigger=trigger, querytype='MATCHALL',
-                            response=response)
-    except Exception as e:
-        instance.cmdwrite('PRIVMSG', (target, 'Error: ' + str(e)))
-        return
+    def handle_privmsg(self, discard, line):
+        if len(line.params) <= 1: return
+        if not line.hostmask: return
 
-    writer.commit()
-    instance.cmdwrite('PRIVMSG', (target, 'Done'))
+        target = self.nickchan_lower(line.params[0])
+        message = line.params[-1]
 
-def handle_quotedel(instance, line, target, params):
-    # XXX delete by ID
-    parsed = re.match('\[(.+)\]', params)
+        # Select the correct target
+        if target[0] not in self.isupport['CHANTYPES']:
+            target = line.hostmask.nick
 
-    if not parsed: return
+        if message.startswith('\x01'):
+            # CTCP stripping
+            message = message.strip('\x01')
+            junk, sep, message = message.partition(' ')
 
-    trigger = parsed.group(1)
-    trigger = trigger.lower()
+        if message.startswith(self.current_nick):
+            # Cut off the nick
+            newmessage = message[len(self.current_nick):]
 
-    writer = ix.writer()
-    try:
-        writer.delete_by_term('trigger', trigger)
-    except Exception as e:
-        instance.cmdwrite('PRIVMSG', (target, 'Error: ' + str(e)))
-        return
+            # Cut off non-alphanum
+            while newmessage and (not newmessage[0].isalnum()):
+                newmessage = newmessage[1:]
 
-    writer.commit()
-    instance.cmdwrite('PRIVMSG', (target, 'Done'))
+            if newmessage:
+                self.handle_command(line, target, newmessage)
+                return
 
-def handle_command(instance, line, target, message):
-    nick = instance.nickchan_lower(line.hostmask.nick)
-
-    if nick not in instance.users: return
-
-    account = instance.users[nick].account
-
-    # No account?
-    if not account or account == '*': return
-
-    if account not in admins: return
-
-    command, sep, params = message.partition(' ')
-    command = command.lower()
-
-    global quitme
-
-    if command == 'add': handle_quoteadd(instance, line, target, params)
-    elif command == 'del': handle_quotedel(instance, line, target, params)
-    elif command == 'quit': quitme = True
-
-def handle_privmsg(instance, line):
-    if len(line.params) <= 1: return
-    if not line.hostmask: return
-
-    target = instance.nickchan_lower(line.params[0])
-    message = line.params[-1]
-
-    # Select the correct target
-    if target[0] not in instance.isupport['CHANTYPES']:
-        target = line.hostmask.nick
-
-    if message.startswith('\x01'):
-        # CTCP stripping
-        message = message.strip('\x01')
-        junk, sep, message = message.partition(' ')
-
-    if message.startswith(instance.current_nick):
-        print("startswith", message)
-        # Cut off the nick
-        newmessage = message[len(instance.current_nick):]
-
-        # Cut off non-alphanum
-        while newmessage and (not newmessage[0].isalnum()):
-            newmessage = newmessage[1:]
-
-        if newmessage:
-            handle_command(instance, line, target, newmessage)
+        # Check last said time
+        if time.time() - self.lastsaid < 15:
             return
 
-    global lastsaid
+        # Lowercase for trigger search
+        message = message.lower()
 
-    # Check last said time
-    if time.time() - lastsaid < 15:
-        return
+        query = make_query(message)
+        searcher = ix.searcher()
+        results = searcher.search(query)
+        print(results)
+        if len(results) == 0: return
 
-    # Lowercase for trigger search
-    message = message.lower()
+        # Best match wins
+        # XXX this logic will need to change at some point to scan for literal
+        # matches...
+        response = results[0]['response']
+        self.cmdwrite('PRIVMSG', (target, response))
 
-    query = make_query(message)
-    searcher = ix.searcher()
-    results = searcher.search(query)
-    print(results)
-    if len(results) == 0: return
+        self.lastsaid = time.time()
 
-    # Best match wins
-    # XXX this logic will need to change at some point to scan for literal
-    # matches...
-    response = results[0]['response']
-    print(response)
-    instance.cmdwrite('PRIVMSG', (target, response))
+    def handle_command(self, line, target, message):
+        nick = self.nickchan_lower(line.hostmask.nick)
 
-    lastsaid = time.time()
+        if nick not in self.users: return
+
+        account = self.users[nick].account
+
+        # No account?
+        if not account or account == '*': return
+
+        if account not in admins: return
+
+        command, sep, params = message.partition(' ')
+        command = command.lower()
+
+        if command == 'add': self.handle_quoteadd(line, target, params)
+        elif command == 'del': self.handle_quotedel(line, target, params)
+        elif command == 'quit': self.quitme()
+    
+    def quitme(self):
+        self.quitme = True
+        self.cmdwrite('QUIT')
+
+    def handle_quoteadd(self, line, target, params):
+        # XXX Different types
+        parsed = re.match('\[(.+)\]\W?=\W?\[(.+)\]', params)
+
+        if not parsed: return
+
+        trigger, response = parsed.groups()
+        trigger = trigger.lower()
+
+        writer = ix.writer()
+        try:
+            writer.add_document(trigger=trigger, querytype='MATCHALL',
+                                response=response)
+        except Exception as e:
+            self.cmdwrite('PRIVMSG', (target, 'Error: ' + str(e)))
+            return
+
+        writer.commit()
+        self.cmdwrite('PRIVMSG', (target, 'Done'))
+
+    def handle_quotedel(self, line, target, params):
+        # XXX delete by ID
+        parsed = re.match('\[(.+)\]', params)
+
+        if not parsed: return
+
+        trigger = parsed.group(1)
+        trigger = trigger.lower()
+
+        writer = ix.writer()
+        try:
+            writer.delete_by_term('trigger', trigger)
+        except Exception as e:
+            self.cmdwrite('PRIVMSG', (target, 'Error: ' + str(e)))
+            return
+
+        writer.commit()
+        self.cmdwrite('PRIVMSG', (target, 'Done'))
+
 
 def run(instance):
     try:
         generator = instance.get_lines()
-        for line in generator:
-            if line.command == "PRIVMSG":
-                handle_privmsg(instance, line)
-
-        if quitme: instance.cmdwrite('QUIT')
+        for line in generator: pass
     except (OSError, IOError) as e:
+        if instance.quitme: quit()
         print("Disconnected", str(e))
         time.sleep(5)
 
@@ -161,8 +164,6 @@ kwargs = {
     'sasl_pw' : 'changeme',
 }
 
-instance = client.IRCClient(**kwargs)
-
 # Initalise the DB or create it
 if not os.path.exists("index"):
     schema = Schema(trigger=TEXT(stored=True, chars=True, vector=True),
@@ -172,11 +173,12 @@ if not os.path.exists("index"):
 else:
     ix = open_dir("index")
 
+instance = SockyIRCClient(**kwargs)
+
 while True:
     try:
         run(instance)
     except BaseException as e:
-        if quitme: quit()
         print('Exception caught:', e)
         instance.terminate()
         raise
